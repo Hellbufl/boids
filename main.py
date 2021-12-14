912268a107980758c0ebce7328565445f2c4e925
Hellbufl <hellbufl@gmail.com>
2021-12-14 16:45:05 +0100

    ~ kleine Änderungen

--- main.py
+++ main.py
@@ -6,20 +6,20 @@ class Boids:
     def __init__(self, n, r):
         self.n = n
         self.radius = r
-        self.agent_positions, self.agent_velocities = self.generate_agents(n)
+        self.agent_positions, self.agent_velocities = self.generate_agents()
 
-    def generate_agents(self, n):
+    def generate_agents(self):
         # Generiert zufällige Positionen und Geschwindigkeiten
         # Gibt Tupel (pos, vel) zurück
         pass
-    
+
     def get_target_mask(self, current_pos):
         # Bestimmt welche Agenten vom aktuellen sichtbar sind
         # Gibt Array aus Bools zurück
         pass
-    
+
     def get_seperation_force(self, current_pos, targets):
-        vectors = current - targets
+        vectors = current_pos - targets
         distances = np.reshape(np.linalg.norm(vectors, axis=1), (len(vectors), 1))
         norm_vectors = vectors / distances
         weighted_vectors = norm_vectors * (self.radius - distances)
@@ -27,7 +27,7 @@ class Boids:
 
     def get_alignment_force(self, current_pos, targets):
         pass
-    
+
     def get_cohesion_force(self, current_pos, targets):
         pass
 
@@ -35,17 +35,17 @@ class Boids:
         # Ändert Geschwindigkeit anhand der wirkenden Kraft
         # Gibt neue Geschwindigkeit zurück
         pass
-    
+
     def update(self):
         # Loop über alle Agenten
         # Bestimmt neuen Status der Simulation
         # Gibt neue Positionen und Geschwindigkeiten zurück
         # WICHTIG: neuen Status in neues Array schreiben. Nicht die Arrays verändern, die noch zur Berechung gebraucht werden!
         pass
-    
+
     def draw(self):
         # Pygame shit
         pass
-    
+
     def mainloop(self):
         pass
